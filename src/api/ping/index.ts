cf9c0b0073d8f7192946132d30d4cc200d746f02
thekidgithub <631046343@qq.com>
2024-06-21 16:59:56 +0800

    1

--- src/api/ping/index.ts
+++ src/api/ping/index.ts
@@ -6,7 +6,7 @@ import request from '../_request';
 
 export async function ping(): Promise<PingResponse> {
   const res: PingResponse = await request({
-    url: '/ping',
+    url: '/v1/ping',
     method: 'GET',
   });
   return res;
