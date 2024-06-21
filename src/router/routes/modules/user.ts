75c1ce8a9f4c68cee0febc0f425be410b7bcd940
thekidgithub <631046343@qq.com>
2024-06-21 22:07:44 +0800

    1

--- src/router/routes/modules/user.ts
+++ src/router/routes/modules/user.ts
@@ -20,21 +20,20 @@ const DASHBOARD = {
         locale: 'menu.user.editInfo',
         requiresAuth: true,
       },
-
     },
   ],
-  beforeEnter(_to: any, _from: any, next: (arg0?: string | undefined) => void) {
-    const res: Promise<PingResponse> = ping();
-    res.then((response) => {
-      if (response !== null) {
-        console.log(res);
-        next();
-      }
-      else next('/login');
-    }).catch((err) => {
-      console.error(err);
-    }); 
-  }
+  // beforeEnter(_to: any, _from: any, next: (arg0?: string | undefined) => void) {
+  //   const res: Promise<PingResponse> = ping();
+  //   res.then((response) => {
+  //     if (response !== null) {
+  //       console.log(res);
+  //       next();
+  //     }
+  //     else next('/login');
+  //   }).catch((err) => {
+  //     console.error(err);
+  //   }); 
+  // }
 };
 
 export default DASHBOARD;
