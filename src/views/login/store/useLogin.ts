9bb2c7f1853ca09b0539f890a0b939e451d61731
thekid <631046343@qq.com>
2024-11-03 16:39:42 +0800

    1

--- src/views/login/store/useLogin.ts
+++ src/views/login/store/useLogin.ts
@@ -50,7 +50,7 @@ const useLoginStore = defineStore('login', {
                   //   else router.push('/user/edit-info');
                   // }
                   // else router.push('/user/edit-info');
-                  window.location.href = `5173.hustunique.com`;
+                  window.location.href = `https://5173.hustunique.com`;
                 }
                 
               });
@@ -78,7 +78,7 @@ const useLoginStore = defineStore('login', {
                   //   else router.push('/user/edit-info');
                   // }
                   // else router.push('/user/edit-info');
-                  window.location.href = `5173.hustunique.com`;
+                  window.location.href = `https://5173.hustunique.com`;
                 }
               });
             }
